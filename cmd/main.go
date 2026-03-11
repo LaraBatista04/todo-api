ee23693151ebd18cc7079612da57d43954621271
LaraBatista04 <100876601+LaraBatista04@users.noreply.github.com>
2026-03-11 00:41:04 -0300

    feat: integração do banco de dados, rotas e logs

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
+// main.go
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	"todo_API/database"
+	"todo_API/routes"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	slog.Info("Iniciando conexão com o banco de dados...")
+	if err := database.Connect(); err != nil {
+		slog.Error("Falha ao inicializar conexão com o banco de dados", "error", err)
+		os.Exit(1)
+	}
+
+	r := gin.Default()
+
+	slog.Info("Configurando rotas da API...")
+	routes.SetupRoutes(r)
+
+	slog.Info("Servidor rodando na porta :8080")
+	if err := r.Run(":8080"); err != nil {
+		slog.Error("Falha ao iniciar o servidor", "error", err)
+		os.Exit(1)
+	}
 }
